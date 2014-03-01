396e0d1e4054e15d588c6bc3791e0096bef1ff69
captaingroup <aditya.gandhi.12@ucl.ac.uk>
2014-03-01 15:55:46 +0000

    Connecting to Database

--- index.php
+++ index.php
@@ -68,7 +68,7 @@
     
     
     
-    <form method="post" action="index.html" enctype="multipart/form-data" >
+    <form method="post" action="index.php" enctype="multipart/form-data" >
       Name  <input type="text" name="name" id="name"/></br>
       Email <input type="text" name="email" id="email"/></br>
       Company <input type="text" name="company" id="company"/></br>
