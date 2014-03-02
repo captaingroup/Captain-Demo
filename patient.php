00643011252a0b95fa3b4d27a92d74d53bdd1845
captaingroup <aditya.gandhi.12@ucl.ac.uk>
2014-03-02 23:57:10 +0000

    Patient : Disabling output buffering

--- patient.php
+++ patient.php
@@ -50,7 +50,7 @@
         }
     } else {
     }
-	/
+	
 	$active = 1;
 	while($active = 1){
 		$result = "SELECT `Device Name`, `Reading` FROM `4-Sensors` WHERE `Patient ID` = ".$patientID." ORDER BY `Time Stamp` DESC LIMIT 1"; 
