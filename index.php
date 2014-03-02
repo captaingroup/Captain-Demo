274b88aed2331f962c28e23ed56d394293c5badc
captaingroup <aditya.gandhi.12@ucl.ac.uk>
2014-03-02 00:02:22 +0000

    CAP : Clickable records

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
     	</div>
 
 		<div class="CAPRows">
-    		<ul id="lsit" class="list">
+    		<ul id="list" class="list">
        			<li id="hey">
            			<p class="patientID">0123456789</p>
            			<p class="patientName">Jonny Walthstow</p>
