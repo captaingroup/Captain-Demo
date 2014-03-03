e8153639684b99bcd5f1a658c812c8c80e430a09
captaingroup <aditya.gandhi.12@ucl.ac.uk>
2014-03-03 23:53:13 +0000

    Patient : Real Time

--- patient.php
+++ patient.php
@@ -4,89 +4,34 @@
 <title>Ajax Add/Delete a Record with jQuery Fade In/Fade Out</title>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
 
+<script language="javascript" src="jquery-1.2.6.min.js"></script>
+<script language="javascript" src="jquery.timers-1.0.0.js"></script>
+
 <script type="text/javascript">
-$(document).ready(function() {
 
-	//##### send add record Ajax request to response.php #########
-	$("#FormSubmit").click(function (e) {
-			e.preventDefault();
-			if($("#contentText").val()==='')
-			{
-				alert("Please enter some text!");
-				return false;
-			}
-		 	var myData = 'content_txt='+ $("#contentText").val(); //build a post data structure
-			jQuery.ajax({
-			type: "POST", // HTTP method POST or GET
-			url: "response.php", //Where to make Ajax calls
-			dataType:"text", // Data type, HTML, json etc.
-			data:myData, //Form variables
-			success:function(response){
-				$("#responds").append(response);
-				$("#contentText").val(''); //empty text field on successful
-			},
-			error:function (xhr, ajaxOptions, thrownError){
-				alert(thrownError);
-			}
-			});
+$(document).ready(function(){
+   var j = jQuery.noConflict();
+	j(document).ready(function()
+	{
+		j(".refreshMe").everyTime(1000,function(i){
+			j.ajax({
+			  url: "refresh-me.php",
+			  cache: false,
+			  success: function(html){
+				j(".refreshMe").html(html);
+			  }
+			})
+		})
 	});
-
-	//##### Send delete Ajax request to response.php #########
-	$("body").on("click", "#responds .del_button", function(e) {
-		 e.returnValue = false;
-		 var clickedID = this.id.split('-'); //Split string (Split works as PHP explode)
-		 var DbNumberID = clickedID[1]; //and get number from array
-		 var myData = 'recordToDelete='+ DbNumberID; //build a post data structure
-		 
-			jQuery.ajax({
-			type: "POST", // HTTP method POST or GET
-			url: "response.php", //Where to make Ajax calls
-			dataType:"text", // Data type, HTML, json etc.
-			data:myData, //Form variables
-			success:function(response){
-				//on success, hide  element user wants to delete.
-				$('#item_'+DbNumberID).fadeOut("slow");
-			},
-			error:function (xhr, ajaxOptions, thrownError){
-				//On error, we alert user
-				alert(thrownError);
-			}
-			});
-	});
-
+   j('.refreshMe').css({color:"red"});
 });
+
+
+
 </script>
 <link href="css/style.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
-<div class="content_wrapper">
-<ul id="responds">
-<?php
-//include db configuration file
-include_once("config.php");
-
-//MySQL query
-$Result = mysql_query("SELECT `Reading` FROM `4-Sensors` WHERE `Patient ID` = 00000000 ORDER BY `Time Stamp` DESC LIMIT 1;");
-
-//get all records from add_delete_record table
-while($row = mysql_fetch_array($Result))
-{
-  echo '<li id="item_'.$row['Reading'].'">';
-  echo '<div class="del_wrapper"><a href="#" class="del_button" id="del-'.$row['Reading'].'">';
-  echo '<img src="images/icon_del.gif" border="0" />';
-  echo '</a></div>';
-  echo $row['Reading'].'</li>';
-}
-
-//close db connection
-mysql_close($connecDB);
-?>
-</ul>
-    <div class="form_style">
-    <textarea name="content_txt" id="contentText" cols="45" rows="5"></textarea>
-    <button id="FormSubmit">Add record</button>
-    </div>
-</div>
-
+<div class="refreshMe">This will get Refreshed in 10 Seconds</div>
 </body>
 </html>
