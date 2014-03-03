4280d797b6cf37906f3ec3467402014d21b9a9dd
captaingroup <aditya.gandhi.12@ucl.ac.uk>
2014-03-03 23:21:44 +0000

    Patient : Real Time Test

--- patient.php
+++ patient.php
@@ -1,74 +1,92 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Patient</title>
-</head>
+<title>Ajax Add/Delete a Record with jQuery Fade In/Fade Out</title>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
 
-<body>
+<script type="text/javascript">
+$(document).ready(function() {
 
-	<?php
-		$patientID = $_GET["id"] ;
-		echo "<h1>Hello " . $_GET["id"] . "</h1>";
-		
-		// DB connection info
-    //TODO: Update the values for $host, $user, $pwd, and $db
-    //using the values you retrieved earlier from the portal.
-    $host = "sql3.freemysqlhosting.net";
-    $user = "sql331497";
-    $pwd = "sI2*yG2*";
-    $db = "sql331497";
-    // Connect to database.
-    try {
-        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    }
-    catch(Exception $e){
-        die(var_dump($e));
-		echo 'did not work';
-    }
-    
-    // Retrieve data
-    $sql_select = "SELECT * FROM `3-Patient` WHERE `Patient ID` = " .$patientID;
-    $stmt = $conn->query($sql_select);
-    $patients = $stmt->fetchAll(); 
-    if(count($patients) > 0) {
-        foreach($patients as $patient) {
-			
-			$patientName = $patient['Patient Name'];
-			$patientAge = $patient['Age'];
-			$medicalCondition = $patient['Medical Condition'];
-			$dateAdmitted = $patient['Date Admitted'];
-			
-			echo $patientID;
-			echo $patientName;
-			echo $patientAge;
-			echo $medicalCondition;
-			echo $dateAdmitted;
-			
-        }
-    } else {
-    }
-	/**
-	$active = 1;
-	while($active = 1){
-		$result = "SELECT `Device Name`, `Reading` FROM `4-Sensors` WHERE `Patient ID` = ".$patientID." ORDER BY `Time Stamp` DESC LIMIT 1"; 
-		$stmt = $conn->query($result);
-    	$readings = $stmt->fetchAll();
-		
-		if(count($readings) > 0) {
-			foreach($readings as $reading) {
-				$sensorName = $reading['Device Name'];
-				$sensorReading = $reading['Reading'];
-				
-				echo $sensorName. " " .$sensorReading;	
+	//##### send add record Ajax request to response.php #########
+	$("#FormSubmit").click(function (e) {
+			e.preventDefault();
+			if($("#contentText").val()==='')
+			{
+				alert("Please enter some text!");
+				return false;
 			}
-		}
-		
-	}*/
-	
-	
-	?>
+		 	var myData = 'content_txt='+ $("#contentText").val(); //build a post data structure
+			jQuery.ajax({
+			type: "POST", // HTTP method POST or GET
+			url: "response.php", //Where to make Ajax calls
+			dataType:"text", // Data type, HTML, json etc.
+			data:myData, //Form variables
+			success:function(response){
+				$("#responds").append(response);
+				$("#contentText").val(''); //empty text field on successful
+			},
+			error:function (xhr, ajaxOptions, thrownError){
+				alert(thrownError);
+			}
+			});
+	});
+
+	//##### Send delete Ajax request to response.php #########
+	$("body").on("click", "#responds .del_button", function(e) {
+		 e.returnValue = false;
+		 var clickedID = this.id.split('-'); //Split string (Split works as PHP explode)
+		 var DbNumberID = clickedID[1]; //and get number from array
+		 var myData = 'recordToDelete='+ DbNumberID; //build a post data structure
+		 
+			jQuery.ajax({
+			type: "POST", // HTTP method POST or GET
+			url: "response.php", //Where to make Ajax calls
+			dataType:"text", // Data type, HTML, json etc.
+			data:myData, //Form variables
+			success:function(response){
+				//on success, hide  element user wants to delete.
+				$('#item_'+DbNumberID).fadeOut("slow");
+			},
+			error:function (xhr, ajaxOptions, thrownError){
+				//On error, we alert user
+				alert(thrownError);
+			}
+			});
+	});
+
+});
+</script>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div class="content_wrapper">
+<ul id="responds">
+<?php
+//include db configuration file
+include_once("config.php");
+
+//MySQL query
+$Result = mysql_query("SELECT * FROM `3-Patient` WHERE `Patient ID` = 00000000");
+
+//get all records from add_delete_record table
+while($row = mysql_fetch_array($Result))
+{
+  echo '<li id="item_'.$row["id"].'">';
+  echo '<div class="del_wrapper"><a href="#" class="del_button" id="del-'.$row["id"].'">';
+  echo '<img src="images/icon_del.gif" border="0" />';
+  echo '</a></div>';
+  echo $row["content"].'</li>';
+}
+
+//close db connection
+mysql_close($connecDB);
+?>
+</ul>
+    <div class="form_style">
+    <textarea name="content_txt" id="contentText" cols="45" rows="5"></textarea>
+    <button id="FormSubmit">Add record</button>
+    </div>
+</div>
 
 </body>
 </html>
