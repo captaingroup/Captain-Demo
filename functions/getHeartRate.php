a16e1664d5afccbc43fc5ba4489d68a5ee1f601b
captaingroup <aditya.gandhi.12@ucl.ac.uk>
2014-03-04 18:25:49 +0000

    Patient : Test Variable ID

--- functions/getHeartRate.php
+++ functions/getHeartRate.php
@@ -17,8 +17,7 @@
     }
     include '../patient.php';
     // Retrieve data
-	echo $patientID;
-    $sql_select = "SELECT `Device Name`, `Reading` FROM `4-Sensors` WHERE `Patient ID` = ".$patientID." ORDER BY `Time Stamp` DESC LIMIT 1";
+    $sql_select = "SELECT `Device Name`, `Reading` FROM `4-Sensors` WHERE `Patient ID` = 00000000 ORDER BY `Time Stamp` DESC LIMIT 1";
     $stmt = $conn->query($sql_select);
     $patients = $stmt->fetchAll(); 
     if(count($patients) > 0) {
@@ -26,6 +25,7 @@
 			
 			echo $patient['Device Name'];
 			echo $patient['Reading'];
+			echo $patientID;
         }
     } else {
     }
