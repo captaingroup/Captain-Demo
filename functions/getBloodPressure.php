750557e50c700d8bed398097e58ed5e12ad0fe97
captaingroup <aditya.gandhi.12@ucl.ac.uk>
2014-03-05 21:14:24 +0000

    Revert "Styling Patient Page"
    This reverts commit 2adcd6dce911d691a7dbdf1a45b6269a8b3f2d32.

--- functions/getBloodPressure.php
+++ functions/getBloodPressure.php
@@ -25,7 +25,7 @@
     if(count($patients) > 0) {
         foreach($patients as $patient) {
 			
-			echo "<h6>".$patient['Reading']."</h6>";
+			echo "<h6>".$patient['Device Name']." ".$patient['Reading']."</h6>";
         }
     } else {
     }
