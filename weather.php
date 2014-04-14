90c49dcbeab2d77f5e255a2168c2d8329b371b9e
captaingroup <aditya.gandhi.12@ucl.ac.uk>
2014-04-14 22:55:08 +0100

    Updated Weather Page

--- weather.php
+++ weather.php
@@ -17,35 +17,6 @@
 
 <script type="text/javascript">
 
-//Get the context of the canvas element we want to select
-var ctx = document.getElementById("myChart").getContext("2d");
-var myNewChart = new Chart(ctx).PolarArea(data);
-new Chart(ctx).Line(data,options);
-
-var data = {
-	labels : ["January","February","March","April","May","June","July"],
-	datasets : [
-		{
-			fillColor : "rgba(220,220,220,0.5)",
-			strokeColor : "rgba(220,220,220,1)",
-			pointColor : "rgba(220,220,220,1)",
-			pointStrokeColor : "#fff",
-			data : [65,59,90,81,56,55,40]
-		},
-		{
-			fillColor : "rgba(151,187,205,0.5)",
-			strokeColor : "rgba(151,187,205,1)",
-			pointColor : "rgba(151,187,205,1)",
-			pointStrokeColor : "#fff",
-			data : [28,48,40,19,96,27,100]
-		}
-	]
-}
-
-
-
-
-
 $(document).ready(function(){
    var j = jQuery.noConflict();
 	j(document).ready(function()
@@ -115,9 +86,39 @@ $(document).ready(function(){
 </head>
 <body>
 
+<canvas id="canvas" height="450" width="600"></canvas>
+
+
+	<script>
+
+		var lineChartData = {
+			labels : ["January","February","March","April","May","June","July"],
+			datasets : [
+				{
+					fillColor : "rgba(220,220,220,0.5)",
+					strokeColor : "rgba(220,220,220,1)",
+					pointColor : "rgba(220,220,220,1)",
+					pointStrokeColor : "#fff",
+					data : [65,59,90,81,56,55,40]
+				},
+				{
+					fillColor : "rgba(151,187,205,0.5)",
+					strokeColor : "rgba(151,187,205,1)",
+					pointColor : "rgba(151,187,205,1)",
+					pointStrokeColor : "#fff",
+					data : [28,48,40,19,96,27,100]
+				}
+			]
+
+		}
+
+	var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Line(lineChartData);
+
+	</script>
+
 <div class="dataContainer">
 	<div class="patientLiveDataContainer">
-    	<div class="ecgGraphData"><canvas id="myChart" width="400" height="400"></canvas></div>
+    	<div class="ecgGraphData"></div>
         <div class="otherDataContainer">
         	<div class="heartRateData"></div>
             <div class="bloodPressureData"></div>
