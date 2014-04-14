b82f73e62679d9fc8bae7136bb0c1b523e1681f5
captaingroup <aditya.gandhi.12@ucl.ac.uk>
2014-04-15 00:24:32 +0100

    Weather

--- functions/live-server-data.php
+++ functions/live-server-data.php
@@ -19,7 +19,7 @@
     }
 		
     // Retrieve data
-    $sql_select = "SELECT `Device Name`, `Reading` FROM `4-Sensors` WHERE `Patient ID` = ".$patientID." AND `Device Name` = 'Heart Rate' ORDER BY `Time Stamp` DESC LIMIT 1";
+    $sql_select = "SELECT `Device Name`, `Reading` FROM `4-Sensors` WHERE `Patient ID` = 00000000 AND `Device Name` = 'Heart Rate' ORDER BY `Time Stamp` DESC LIMIT 1";
     $stmt = $conn->query($sql_select);
     $patients = $stmt->fetchAll(); 
     
