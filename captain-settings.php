eceb5dbb758993ef64473c426dd82f6a55b6859c
captaingroup <aditya.gandhi.12@ucl.ac.uk>
2014-04-22 17:30:51 +0100

    Settings Website

---
+++ captain-settings.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Captain - Temperature Example</title>
+<link href="css/stylesheet.css" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Nova+Slim|Parisienne|Nova+Mono|Pacifico|Comfortaa|Varela+Round|Dr+Sugiyama' rel='stylesheet' type='text/css'>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	
+    <div class="formContainer">
+    	<form id="login">
+    	<h1>Log In</h1>
+    	<fieldset id="inputs">
+        	<input id="username" type="text" placeholder="Username" autofocus required>   
+        	<input id="password" type="password" placeholder="Password" required>
+    	</fieldset>
+    	<fieldset id="actions">
+        	<input type="submit" id="submit" value="Log in">
+        	<a href="">Forgot your password?</a><a href="">Register</a>
+    	</fieldset>
+	</form>
+    </div>   
+
+</body>
+</html>
