1516e8f9b9e970179a1dcc766d0b2f11bb4de93a
captaingroup <aditya.gandhi.12@ucl.ac.uk>
2014-04-24 17:50:17 +0100

    Weather Styles

--- weather.php
+++ weather.php
@@ -3,7 +3,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Captain - Temperature Example</title>
 <link href="css/stylesheet.css" rel="stylesheet">
-<link href='http://fonts.googleapis.com/css?family=Pacifico|Comfortaa:700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
 <script src="http://code.highcharts.com/highcharts.js"></script>
 <script src="http://code.highcharts.com/modules/exporting.js"></script>
