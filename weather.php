ac3546b50d4aace05eeda74e3042f788356526d8
captaingroup <aditya.gandhi.12@ucl.ac.uk>
2014-04-26 16:26:05 +0100

    dd

--- weather.php
+++ weather.php
@@ -162,6 +162,7 @@
 			<div class="fleft">
 				<select id="cd-dropdown" name="cd-dropdown" class="cd-select" onChange="onOptionChange()">
 					<option value="-1" selected>Select Sensor Group</option>
+                    <option value="1" class="icon-monkey">Monkey</option>
 				</select>
 			</div>
 		</section>
