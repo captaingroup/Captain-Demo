9dab3f7541c1953f1cfad5c76eef6a143300e4c4
captaingroup <aditya.gandhi.12@ucl.ac.uk>
2014-04-26 14:46:40 +0100

    weather drop down

--- weather.php
+++ weather.php
@@ -4,7 +4,6 @@
 <title>Captain - Temperature Example</title>
 <link href="css/stylesheet.css" rel="stylesheet">
 <link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
 <script src="http://code.highcharts.com/highcharts.js"></script>
 <script src="http://code.highcharts.com/modules/exporting.js"></script>
 <script src="javascript/jquery-1.10.2.min.js"></script>
