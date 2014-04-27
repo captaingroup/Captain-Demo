f41473fe45959fc008a59dc837068ad6f51a6b13
captaingroup <aditya.gandhi.12@ucl.ac.uk>
2014-04-27 18:12:30 +0100

    Scripts

--- weather.php
+++ weather.php
@@ -32,55 +32,7 @@
 
 
 <script>
-	var chart;
-	function requestData() {
-        $.ajax({
-            url: 'functions/live-server-data.php?id=<?php echo $patientID;?>', 
-            success: function(point) {
-                var series = chart.series[0],
-                    shift = series.data.length > 20; // shift if the series is longer than 20
-
-                // add the point
-                chart.series[0].addPoint(eval(point), true, shift);
-
-                // call it again after one second
-                setTimeout(requestData, 1000);  
-            },
-            cache: false
-        });
-    }
-    $(document).ready(function() {
-        chart = new Highcharts.Chart({
-            chart: {
-                renderTo: 'container',
-                defaultSeriesType: 'spline',
-                events: {
-                    load: requestData
-                }
-            },
-            title: {
-                text: 'Live Temperature Data'
-            },
-            xAxis: {
-                type: 'datetime',
-                tickPixelInterval: 150,
-                maxZoom: 20 * 1000
-            },
-            yAxis: {
-                minPadding: 0.2,
-                maxPadding: 0.2,
-                title: {
-                    text: 'Value',
-                    margin: 80
-                }
-            },
-            series: [{
-                name: 'Random data',
-                data: []
-            }]
-        });     
-    });
-</script>
+	</script>
 	
     
 	<script>			
@@ -157,7 +109,64 @@
 		</section>
 	</div><!-- /container -->
         
-    <script type="text/javascript">		
+    <script type="text/javascript">	
+	
+	
+	var chart;
+	function requestData() {
+        $.ajax({
+            url: 'functions/live-server-data.php?id=<?php echo $patientID;?>', 
+            success: function(point) {
+                var series = chart.series[0],
+                    shift = series.data.length > 20; // shift if the series is longer than 20
+
+                // add the point
+                chart.series[0].addPoint(eval(point), true, shift);
+
+                // call it again after one second
+                setTimeout(requestData, 1000);  
+            },
+            cache: false
+        });
+    }
+    $(document).ready(function() {
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'container',
+                defaultSeriesType: 'spline',
+                events: {
+                    load: requestData
+                }
+            },
+            title: {
+                text: 'Live Temperature Data'
+            },
+            xAxis: {
+                type: 'datetime',
+                tickPixelInterval: 150,
+                maxZoom: 20 * 1000
+            },
+            yAxis: {
+                minPadding: 0.2,
+                maxPadding: 0.2,
+                title: {
+                    text: 'Value',
+                    margin: 80
+                }
+            },
+            series: [{
+                name: 'Random data',
+                data: []
+            }]
+        });     
+    });
+
+	
+	
+	
+	
+	
+		
 		$( function() {	
 			$( '#cd-dropdown' ).dropdown( {
 				gutter : 5,
