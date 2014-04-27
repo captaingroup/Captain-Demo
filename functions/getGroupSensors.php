21ccbd397dfba527dec0e471bf6c2bfd8310d84a
captaingroup <aditya.gandhi.12@ucl.ac.uk>
2014-04-27 16:25:56 +0100

    dd

--- functions/getGroupSensors.php
+++ functions/getGroupSensors.php
@@ -28,7 +28,7 @@
 
    if(count($groupIDs) > 0) {
         foreach($groupIDs as $groupID) {
-			$sql_select2 = "SELECT `SensorID` FROM `SensorGroup` WHERE `GroupID` = ".$groupID." ";
+			$sql_select2 = "SELECT `SensorID` FROM `SensorGroup` WHERE `GroupID` = ".$groupID;
    			$stmt2 = $conn->query($sql_select2);
     		$sensorIDs = $stmt2->fetchAll(); 
 			if(count($sensorIDs) > 0) {
